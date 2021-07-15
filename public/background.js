113fdcc8b2041434b4c149405e02978c9035832f
Christopher Owen Prawira <cpra0004@student.monash.edu>
2021-07-15 11:51:25 +1000

    Alter popup and background to display name correctly

--- public/background.js
+++ public/background.js
@@ -1,5 +1,7 @@
 /* global chrome */
 chrome.identity.getAuthToken({ interactive: true }, function (token) {
-	const url = `http://localhost:8080/auth/chrome?access_token=${token}`;
-	fetch(url).then((response) => response.json());
+	chrome.storage.sync.set({ token: token }, () => {
+		console.log('Token set to ' + token);
+		// create a new account in DB if user logged in for first time
+	});
 });
