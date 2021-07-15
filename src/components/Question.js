0e4bc075f746b4781614b7d22d52b69d83ae95f9
Christopher Owen Prawira <cpra0004@student.monash.edu>
2021-07-15 16:08:38 +1000

    Add missing author key to default state

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,41 +1,37 @@
-import QuestionHeader from './QuestionHeader'
-import QuestionTitle from './QuestionTitle'
-import { Sidebar } from './Sidebar'
+import QuestionHeader from './QuestionHeader';
+import QuestionTitle from './QuestionTitle';
+import { Sidebar } from './Sidebar';
 import styled from 'styled-components';
-import QuestionContent from './QuestionContent'
-import QuestionFooter from './QuestionFooter'
+import QuestionContent from './QuestionContent';
+import QuestionFooter from './QuestionFooter';
 
 import { useContext } from 'react';
-import { QuestionContext } from './QuestionContext'
+import { QuestionContext } from './QuestionContext';
 
 const CustomDiv = styled.div`
-    display: flex;
-    flex-direction:row;
-`
-
+	display: flex;
+	flex-direction: row;
+`;
 
 function Question() {
-    const {question, setQuestion} = useContext(QuestionContext)
+	const { question, setQuestion } = useContext(QuestionContext);
 
-
-
-    return (
-            <CustomDiv>
-                <Sidebar />
-                <div className='questionMain'>
-                    <QuestionHeader
-                        userName={question.author}
-                        timeStamp={question.timestamp}
-                        dateUploaded={question.dateCreated}
-                        userImageUrl={question.author} />
-                    <QuestionTitle questionTitle={question.title} />
-                    <QuestionContent
-                        question={question.content}
-                    />
-                    <QuestionFooter totalReplies={question.repliesCount} />
-                </div>
-            </CustomDiv>
-    )
+	return (
+		<CustomDiv>
+			<Sidebar />
+			<div className="questionMain">
+				<QuestionHeader
+					userName={question.author.given_name}
+					timeStamp={question.timestamp}
+					dateUploaded={question.dateCreated}
+					userImageUrl={question.author.picture}
+				/>
+				<QuestionTitle questionTitle={question.title} />
+				<QuestionContent question={question.content} />
+				<QuestionFooter totalReplies={question.content} />
+			</div>
+		</CustomDiv>
+	);
 }
 
-export default Question
+export default Question;
