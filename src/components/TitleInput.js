dca7c34da6f9ae0c14a873b7554e56bbfa065a83
ansh <bajpaiansh8@gmail.com>
2021-07-23 21:22:24 +0530

    Minor change

--- src/components/TitleInput.js
+++ src/components/TitleInput.js
@@ -1,8 +1,8 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import styled from 'styled-components';
 
 const CustomDiv = styled.div`
-    width: 100%;
+    min-width: 100%;
     height: 50px;
     display: flex;
     flex-direction: column;
@@ -27,39 +27,39 @@ function TitleInput({ title, setTitle }) {
 
 
     const [focus, setFocus] = useState(false)
-    
-    
+
+
     var hotkeys = document.getElementsByTagName('yt-Hotkey-Manager')[0]
     var hotkeysParent = hotkeys ? hotkeys.parentNode : false;
 
-    function addYouTubeHotkeys(){
-        if(hotkeysParent)
-        hotkeysParent.appendChild(hotkeys);
+    function addYouTubeHotkeys() {
+        if (hotkeysParent)
+            hotkeysParent.appendChild(hotkeys);
     }
 
-    function removeYoutubeHotkeys(){    
-            var x = document.getElementsByTagName('yt-Hotkey-Manager')[0]
-            if(x){
-                var clone = x.cloneNode(false)
-                x.parentNode.replaceChild(clone,x)
-                clone.parentNode.removeChild(clone)         
-            }
+    function removeYoutubeHotkeys() {
+        var x = document.getElementsByTagName('yt-Hotkey-Manager')[0]
+        if (x) {
+            var clone = x.cloneNode(false)
+            x.parentNode.replaceChild(clone, x)
+            clone.parentNode.removeChild(clone)
         }
-    
+    }
+
     useEffect(() => {
-        if(focus){
-                removeYoutubeHotkeys()
-            }
-            else{
-                addYouTubeHotkeys()
-            }  
+        if (focus) {
+            removeYoutubeHotkeys()
+        }
+        else {
+            addYouTubeHotkeys()
+        }
     })
 
     return (
         <CustomDiv>
-            <CustomInput 
-                type="text" 
-                placeholder="What's your question ?? " 
+            <CustomInput
+                type="text"
+                placeholder="What's your question ?? "
                 onChange={(e) => setTitleText(e)}
                 onFocus={() => setFocus(true)}
                 onBlur={() => setFocus(false)}></CustomInput>
