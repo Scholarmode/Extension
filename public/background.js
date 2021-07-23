ca0987d798852a73d33195cfcccd560229e267ef
Christopher Owen Prawira <cpra0004@student.monash.edu>
2021-07-24 00:12:50 +1000

    Delete packagelock json

--- public/background.js
+++ public/background.js
@@ -1,7 +1,4 @@
 /* global chrome */
 chrome.identity.getAuthToken({ interactive: true }, function (token) {
-	chrome.storage.sync.set({ token: token }, () => {
-		console.log('Token set to ' + token);
-		// create a new account in DB if user logged in for first time
-	});
+	chrome.storage.sync.set({ token: token }, () => {});
 });
