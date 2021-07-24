39608496361fb6d9d77292acbab5f5c06d697386
Christopher Owen Prawira <cpra0004@student.monash.edu>
2021-07-24 17:39:14 +1000

    Add heroku port

--- src/server/app.js
+++ src/server/app.js
@@ -1,118 +1,119 @@
-require('dotenv').config();
-const passport = require('passport');
-const cookieSession = require('cookie-session');
-const GoogleTokenStrategy = require('passport-google-token').Strategy;
-const Account = require('./models/account');
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+require('dotenv').config()
+const passport = require('passport')
+const cookieSession = require('cookie-session')
+const GoogleTokenStrategy = require('passport-google-token').Strategy
+const Account = require('./models/account')
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
 
-const accounts = require('./routers/account');
-const questions = require('./routers/question');
-const replies = require('./routers/reply');
-const app = express();
+const accounts = require('./routers/account')
+const questions = require('./routers/question')
+const replies = require('./routers/reply')
+const app = express()
+const port = process.env.PORT || 8080
 
 // Authenticate with Google Token Strategy
 passport.use(
-	new GoogleTokenStrategy(
-		{
-			clientID: process.env.G_CLIENT_ID,
-			clientSecret: process.env.G_CLIENT_SECRET,
-		},
-		(accessToken, refreshToken, profile, done) => {
-			Account.findOne({ googleId: profile.id }).then((currentUser) => {
-				if (currentUser) {
-					done(null, currentUser);
-				} else {
-					// Save to the account collection
-					let newAccountDetails = profile._json;
-					newAccountDetails._id = new mongoose.Types.ObjectId();
-					newAccountDetails.googleId = profile.id;
+    new GoogleTokenStrategy(
+        {
+            clientID: process.env.G_CLIENT_ID,
+            clientSecret: process.env.G_CLIENT_SECRET,
+        },
+        (accessToken, refreshToken, profile, done) => {
+            Account.findOne({ googleId: profile.id }).then((currentUser) => {
+                if (currentUser) {
+                    done(null, currentUser)
+                } else {
+                    // Save to the account collection
+                    let newAccountDetails = profile._json
+                    newAccountDetails._id = new mongoose.Types.ObjectId()
+                    newAccountDetails.googleId = profile.id
 
-					let account = new Account(newAccountDetails);
-					account.save((newUser) => {
-						done(null, newUser);
-					});
-				}
-			});
-		}
-	)
-);
+                    let account = new Account(newAccountDetails)
+                    account.save((newUser) => {
+                        done(null, newUser)
+                    })
+                }
+            })
+        }
+    )
+)
 
 passport.serializeUser((user, done) => {
-	done(null, user.id);
-});
+    done(null, user.id)
+})
 
 passport.deserializeUser((id, done) => {
-	Account.findById(id).then((user) => {
-		done(null, user);
-	});
-});
+    Account.findById(id).then((user) => {
+        done(null, user)
+    })
+})
 
-app.listen(8080);
-console.log('Listening on port 8080');
+app.listen(port)
+console.log(`Listening on port ${port}`)
 
 app.use(
-	cookieSession({
-		maxAge: 24 * 60 * 60 * 1000, // 1 day
-		keys: [process.env.S_COOKIE_KEY],
-	})
-);
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+    cookieSession({
+        maxAge: 24 * 60 * 60 * 1000, // 1 day
+        keys: [process.env.S_COOKIE_KEY],
+    })
+)
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
-app.use(cors());
+app.use(cors())
 
-const atlasUser = process.env.ATLAS_USER;
-const atlasPwrd = process.env.ATLAS_PWRD;
+const atlasUser = process.env.ATLAS_USER
+const atlasPwrd = process.env.ATLAS_PWRD
 
-const uri = `mongodb+srv://${atlasUser}:${atlasPwrd}@realmcluster.fi10q.mongodb.net/ScholarMode?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${atlasUser}:${atlasPwrd}@realmcluster.fi10q.mongodb.net/ScholarMode?retryWrites=true&w=majority`
 
 mongoose.connect(
-	uri,
-	{
-		useUnifiedTopology: true,
-		useNewUrlParser: true,
-		useFindAndModify: false,
-	},
-	function (err) {
-		if (err) {
-			return console.log('Mongoose - connection error:', err);
-		}
-		console.log('Connected to Mongoose successfully');
-	}
-);
+    uri,
+    {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useFindAndModify: false,
+    },
+    function (err) {
+        if (err) {
+            return console.log('Mongoose - connection error:', err)
+        }
+        console.log('Connected to Mongoose successfully')
+    }
+)
 
 //Configuring Endpoints
 // Account RESTFul endpoints
-app.get('/accounts', accounts.getAll);
-app.get('/accounts/:id', accounts.getOne);
-app.post('/accounts', accounts.createOne);
-app.put('/accounts/:id', accounts.updateOne);
-app.delete('/accounts/:id', accounts.deleteOne);
+app.get('/accounts', accounts.getAll)
+app.get('/accounts/:id', accounts.getOne)
+app.post('/accounts', accounts.createOne)
+app.put('/accounts/:id', accounts.updateOne)
+app.delete('/accounts/:id', accounts.deleteOne)
 
 // Question RESTFul endpoints
-app.get('/questions', questions.getAll);
-app.get('/questions/:id', questions.getOne);
-app.get('/questions/author/:id', questions.getAuthorQuestions);
-app.post('/questions', questions.createOne);
-app.put('/questions/:id', questions.updateOne);
-app.delete('/questions/:id', questions.deleteOne);
-app.put('/questions/:id/upvote', questions.upvote);
-app.put('/questions/:id/downvote', questions.downvote);
+app.get('/questions', questions.getAll)
+app.get('/questions/:id', questions.getOne)
+app.get('/questions/author/:id', questions.getAuthorQuestions)
+app.post('/questions', questions.createOne)
+app.put('/questions/:id', questions.updateOne)
+app.delete('/questions/:id', questions.deleteOne)
+app.put('/questions/:id/upvote', questions.upvote)
+app.put('/questions/:id/downvote', questions.downvote)
 
 // Reply RESTFul endpoints
-app.get('/replies', replies.getAll);
-app.get('/replies/:id', replies.getOne);
-app.post('/replies', replies.createOne);
-app.put('/replies/:id', replies.updateOne);
-app.delete('/replies/:id', replies.deleteOne);
-app.put('/replies/:id/upvote', replies.upvote);
-app.put('/replies/:id/downvote', replies.downvote);
+app.get('/replies', replies.getAll)
+app.get('/replies/:id', replies.getOne)
+app.post('/replies', replies.createOne)
+app.put('/replies/:id', replies.updateOne)
+app.delete('/replies/:id', replies.deleteOne)
+app.put('/replies/:id/upvote', replies.upvote)
+app.put('/replies/:id/downvote', replies.downvote)
 
 // Google AUTH endpoints
 app.get('/auth/chrome', passport.authenticate('google-token'), (req, res) => {
-	res.json(req.user);
-});
+    res.json(req.user)
+})
