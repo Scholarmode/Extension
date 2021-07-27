87b690c7e96b0e952e8a99b1f2422b5b84202200
Christopher Owen Prawira <cpra0004@student.monash.edu>
2021-07-27 14:44:41 +1000

    Populate author fields

--- src/server/routers/question.js
+++ src/server/routers/question.js
@@ -1,117 +1,138 @@
-const mongoose = require('mongoose');
-const Question = require('../models/question');
-const Replies = require('../models/reply');
+const mongoose = require('mongoose')
+const Question = require('../models/question')
+const Replies = require('../models/reply')
 
 module.exports = {
-	getAll: (req, res) => {
-		Question.find((err, questions) => {
-			if (err) return res.status(400).json(err);
-			if (!questions) return res.status(404).json();
+    getAll: (req, res) => {
+        // Question.find((err, questions) => {
+        //     if (err) return res.status(400).json(err)
+        //     if (!questions) return res.status(404).json()
 
-			if (questions.length === 0)
-				return res.status(404).json('No matching documents');
-			res.json(questions);
-		});
-	},
+        //     if (questions.length === 0)
+        //         return res.status(404).json('No matching documents')
+        //     res.json(questions)
+        // })
 
-	createOne: (req, res) => {
-		let newQuestionDetails = req.body;
-		newQuestionDetails._id = new mongoose.Types.ObjectId();
+        Question.find()
+            .populate('author')
+            .populate({
+                path: 'replies',
+                populate: [
+                    { path: 'author' },
+                    {
+                        path: 'replies',
+                        populate: [{ path: 'author' }, { path: 'replies' }],
+                    },
+                ],
+            })
+            .exec((err, questions) => {
+                if (err) return res.status(400).json(err)
+                if (!questions) return res.status(404).json()
 
-		let question = new Question(newQuestionDetails);
-		question.save((err) => {
-			if (err) return res.status(400).json(err);
-			res.json(question);
-		});
-	},
+                if (questions.length === 0)
+                    return res.status(404).json('No matching documents')
+                res.json(questions)
+            })
+    },
 
-	getOne: (req, res) => {
-		Question.findOne({ _id: req.params.id })
-			.populate('author')
-			.populate({
-				path: 'replies',
-				populate: [
-					{ path: 'author' },
-					{
-						path: 'replies',
-						populate: [{ path: 'author' }, { path: 'replies' }],
-					},
-				],
-			})
-			.exec((err, question) => {
-				if (err) return res.status(400).json(err);
-				res.json(question);
-			});
-	},
+    createOne: (req, res) => {
+        let newQuestionDetails = req.body
+        newQuestionDetails._id = new mongoose.Types.ObjectId()
 
-	getAuthorQuestions: (req, res) => {
-		// Question.find({ author: req.params.id }, (err, questions) => {
-		// 	if (err) return res.status(400).json(err);
-		// 	if (!questions) return res.status(404).json();
-		// 	return res.json(questions);
-		// });
-		Question.find({ author: req.params.id })
-			.populate('author')
-			.populate({
-				path: 'replies',
-				populate: [{ path: 'replies' }],
-			})
-			.exec((err, questions) => {
-				if (err) return res.status(400).json(err);
-				if (!questions) return res.status(404).json();
-				return res.json(questions);
-			});
-	},
+        let question = new Question(newQuestionDetails)
+        question.save((err) => {
+            if (err) return res.status(400).json(err)
+            res.json(question)
+        })
+    },
 
-	updateOne: (req, res) => {
-		Question.findOneAndUpdate(
-			{ _id: req.params.id },
-			req.body,
-			{ returnOriginal: false },
-			(err, question) => {
-				if (err) return res.status(400).json(err);
-				if (!question) return res.status(404).json();
-				res.json(question);
-			}
-		);
-	},
+    getOne: (req, res) => {
+        Question.findOne({ _id: req.params.id })
+            .populate('author')
+            .populate({
+                path: 'replies',
+                populate: [
+                    { path: 'author' },
+                    {
+                        path: 'replies',
+                        populate: [{ path: 'author' }, { path: 'replies' }],
+                    },
+                ],
+            })
+            .exec((err, question) => {
+                if (err) return res.status(400).json(err)
+                res.json(question)
+            })
+    },
 
-	deleteOne: (req, res) => {
-		// Delete replies of the question
-		Replies.deleteMany({ parentQuestion: req.params.id }, (err) => {
-			if (err) return res.status(400).json(err);
+    getAuthorQuestions: (req, res) => {
+        // Question.find({ author: req.params.id }, (err, questions) => {
+        // 	if (err) return res.status(400).json(err);
+        // 	if (!questions) return res.status(404).json();
+        // 	return res.json(questions);
+        // });
+        Question.find({ author: req.params.id })
+            .populate('author')
+            .populate({
+                path: 'replies',
+                populate: [{ path: 'replies' }],
+            })
+            .exec((err, questions) => {
+                if (err) return res.status(400).json(err)
+                if (!questions) return res.status(404).json()
+                return res.json(questions)
+            })
+    },
 
-			// Delete the question itself
-			Question.findOneAndRemove({ _id: req.params.id }, (err) => {
-				if (err) return res.status(400).json(err);
-				res.json();
-			});
-		});
-	},
+    updateOne: (req, res) => {
+        Question.findOneAndUpdate(
+            { _id: req.params.id },
+            req.body,
+            { returnOriginal: false },
+            (err, question) => {
+                if (err) return res.status(400).json(err)
+                if (!question) return res.status(404).json()
+                res.json(question)
+            }
+        )
+    },
 
-	upvote: (req, res) => {
-		Question.findOneAndUpdate(
-			{ _id: req.params.id },
-			{ $inc: { votes: 1 } },
-			{ returnOriginal: false },
-			(err, question) => {
-				if (err) return res.status(400).json(err);
-				if (!question) return res.status(404).json();
-				res.json(question);
-			}
-		);
-	},
+    deleteOne: (req, res) => {
+        // Delete replies of the question
+        Replies.deleteMany({ parentQuestion: req.params.id }, (err) => {
+            if (err) return res.status(400).json(err)
 
-	downvote: (req, res) => {
-		Question.findOneAndUpdate(
-			{ _id: req.params.id },
-			{ $inc: { votes: -1 } },
-			{ returnOriginal: false },
-			(err, question) => {
-				if (err) return res.status(400).json(err);
-				if (!question) return res.status(404).json();
-				res.json(question);
-			}
-		);
-	},
-};
+            // Delete the question itself
+            Question.findOneAndRemove({ _id: req.params.id }, (err) => {
+                if (err) return res.status(400).json(err)
+                res.json()
+            })
+        })
+    },
+
+    upvote: (req, res) => {
+        Question.findOneAndUpdate(
+            { _id: req.params.id },
+            { $inc: { votes: 1 } },
+            { returnOriginal: false },
+            (err, question) => {
+                if (err) return res.status(400).json(err)
+                if (!question) return res.status(404).json()
+                res.json(question)
+            }
+        )
+    },
+
+    downvote: (req, res) => {
+        Question.findOneAndUpdate(
+            { _id: req.params.id },
+            { $inc: { votes: -1 } },
+            { returnOriginal: false },
+            (err, question) => {
+                if (err) return res.status(400).json(err)
+                if (!question) return res.status(404).json()
+                res.json(question)
+            }
+        )
+    },
+}
