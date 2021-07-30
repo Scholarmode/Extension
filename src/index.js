90df2e6695732bd8a88f2197f192e2ddb64eab97
chenga3 <a.cheng13@student.unimelb.edu.au>
2021-07-30 13:26:50 +0800

    prettier formatting

--- src/index.js
+++ src/index.js
@@ -10,48 +10,48 @@ insertionPoint.id = 'insertion-point'
 
 //check for pc searchbar and append scholarmode
 if (document.querySelector('#center')) {
-  document.querySelector('#center').appendChild(insertionPoint)
+    document.querySelector('#center').appendChild(insertionPoint)
 }
 
 //check for mobile searchbar and append scholarmode !doesn't work because m.youtube.com 404 error
 else if (
-  document.querySelector('.mobile-topbar-header-content non-search-mode cbox')
+    document.querySelector('.mobile-topbar-header-content non-search-mode cbox')
 ) {
-  document
-    .querySelector('.mobile-topbar-header-content non-search-mode cbox')
-    .appendChild(insertionPoint)
+    document
+        .querySelector('.mobile-topbar-header-content non-search-mode cbox')
+        .appendChild(insertionPoint)
 } else {
-  console.log('scholarmode not rendered')
+    console.log('scholarmode not rendered')
 }
 
 //render ScholarMode
 ReactDOM.render(
-  <React.StrictMode>
-    <ScholarModeButton />
-  </React.StrictMode>,
-  document.getElementById('insertion-point')
+    <React.StrictMode>
+        <ScholarModeButton />
+    </React.StrictMode>,
+    document.getElementById('insertion-point')
 )
 
-
 //create div for discussion
-const beforeVideos = document.createElement('div');
-beforeVideos.id = 'prevideos';
-beforeVideos.style.display = 'none';
+const beforeVideos = document.createElement('div')
+beforeVideos.id = 'prevideos'
+beforeVideos.style.display = 'none'
 
 //check for recommended videos and place div before videos
 if (document.querySelector('#secondary')) {
-  document.querySelector('#secondary').insertAdjacentElement('afterbegin', beforeVideos)
-}
-else {
-  console.log('discussion div not rendered')
+    document
+        .querySelector('#secondary')
+        .insertAdjacentElement('afterbegin', beforeVideos)
+} else {
+    console.log('discussion div not rendered')
 }
 
 // render discussion into hidden div
 ReactDOM.render(
-  <React.StrictMode>
-    <Discussion />
-  </React.StrictMode>,
-  document.getElementById('prevideos')
+    <React.StrictMode>
+        <Discussion />
+    </React.StrictMode>,
+    document.getElementById('prevideos')
 )
 
 //find recommended videos
@@ -60,36 +60,30 @@ const videos = document.getElementById('secondary-inner')
 //find toggle to know the state
 const toggle = document.querySelector('.sc-bdnxRM')
 
-
 //observe when toggle changes
-const toggleObserver = new MutationObserver(
-  function(mutations){
-    mutations.forEach(function(mutation){
-        if(mutation.target.classList[2] === 'active'){
-            beforeVideos.style.display = "block"
-            videos.style.display = "none";
-          }else{
-            beforeVideos.style.display = "none"
-            videos.style.display = "block"
-          }
-      })
-  }
-)
+const toggleObserver = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
+        if (mutation.target.classList[2] === 'active') {
+            beforeVideos.style.display = 'block'
+            videos.style.display = 'none'
+        } else {
+            beforeVideos.style.display = 'none'
+            videos.style.display = 'block'
+        }
+    })
+})
 
 toggleObserver.observe(toggle, {
-  childList: false,
-  attributes: true
+    childList: false,
+    attributes: true,
 })
-    
-  
-
 
 // update recommended videos on first YouTube load or any refresh
 // window.onload = () => {
 //     if (toggle.classList[toggle.classList.length - 1] === 'active') {
 //       beforeVideos.style.display = "block"
 //       videos.style.display = "none";
-      
+
 //     } else {
 //       beforeVideos.style.display = "none"
 //       videos.style.display = "block"
