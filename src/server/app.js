93919697c99f43f9158e267103c42a0de04f699c
Ansh Bajpai <65030524+anshbajpai@users.noreply.github.com>
2021-08-01 13:44:01 +0530

    Updated app.js

--- src/server/app.js
+++ src/server/app.js
@@ -3,7 +3,11 @@ const loaders = require('./loaders')
 const cookieSession = require('cookie-session')
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
 
+const accounts = require('./routers/account')
+const questions = require('./routers/question')
+const replies = require('./routers/reply')
 const app = express()
 const port = process.env.PORT || 8080
 
@@ -17,7 +21,30 @@ app.use(
     })
 )
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 app.use(cors())
+
+const atlasUser = process.env.ATLAS_USER
+const atlasPwrd = process.env.ATLAS_PWRD
+
+const uri = `mongodb+srv://${atlasUser}:${atlasPwrd}@realmcluster.fi10q.mongodb.net/ScholarMode?retryWrites=true&w=majority`
+
+mongoose.connect(
+    uri,
+    {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useFindAndModify: false,
+    },
+    function (err) {
+        if (err) {
+            return console.log('Mongoose - connection error:', err)
+        }
+        console.log('Connected to Mongoose successfully')
+    }
+)
 
 loaders(app)
 
@@ -54,3 +81,4 @@ app.put('/replies/:id/:accountId/downvote', replies.downvote)
 app.put('/replies/:id/:accountId/unvote', replies.removeVote)
 app.put('/replies/:id/:accountId/report', replies.report)
 app.delete('/replies/:id/:accountId/report', replies.removeReport)
+
