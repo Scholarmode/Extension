bf68be3aac0253891f948fef1658f842b1b56af4
SJcoded <who@sjcoded.com>
2021-08-21 03:20:27 +1000

    more style changes

--- src/components/VotedTagContainer.js
+++ src/components/VotedTagContainer.js
@@ -2,12 +2,11 @@ import React from 'react'
 import styled from 'styled-components'
 
 const TagContainer = styled.div `
-    height: 40px;
+    height: 60px;
     width: 174px;
     border: 1px solid #DADADA;
     box-sizing: border-box;
     border-radius: 6px 6px 0px 0px;
-    background-color: black;
     position: relative;
     bottom: -9px;
 `
