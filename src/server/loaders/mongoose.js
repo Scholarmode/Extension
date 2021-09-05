62694cb695b08c0da8ac3112183876d1afce998d
Christopher Owen Prawira <cpra0004@student.monash.edu>
2021-09-06 00:24:21 +1000

    Change to production DB

--- src/server/loaders/mongoose.js
+++ src/server/loaders/mongoose.js
@@ -8,7 +8,7 @@ module.exports = async (expressApp) => {
     // const uri = `mongodb+srv://${atlasUser}:${atlasPwrd}@realmcluster.fi10q.mongodb.net/ScholarMode?retryWrites=true&w=majority`
     
     //production DB
-    const uri = `mongodb+srv://${atlasUser}:${atlasPwrd}@realmcluster.oipeo.mongodb.net/ScholarMode?retryWrites=true&w=majority`
+    const uri = `mongodb+srv://${atlasUser}:${atlasPwrd}@scholarmodeprod.oipeo.mongodb.net/ScholarMode?retryWrites=true&w=majority`
 
     mongoose.connect(
         uri,
