930f707120c63fff7c43d3ef107a8fc0808fd5a9
SJ coded <who@sjcoded.com>
2021-10-03 05:41:50 +1100

    clean up comments

--- src/components/Replies.js
+++ src/components/Replies.js
@@ -1,8 +1,5 @@
-import { useState } from 'react';
 import styled from 'styled-components';
 import Reply from './Reply';
-import { useContext } from 'react';
-import { QuestionContext } from './QuestionContext';
 
 const CustomDiv = styled.div`
 	display: flex;
@@ -21,54 +18,7 @@ const CustomUnorderedList = styled.ul`
 `;
 
 function Replies(props) {
-	//const { question, setQuestion } = useContext(QuestionContext);
-
-
-	// const nest = (items, id = null) =>
-	// 	items.filter(item => item.parentReply === id)
-	// 		.map(item => ({ ...item, children: nest(items.replies, item._id) }));
-
-	// const nest = (items, id = null) =>
-	// 	items
-	// 		.filter((item) => item.parentReply === id)
-	// 		.map((item) => ({
-	// 			...item,
-	// 			children: nest(items.replies, item._id),
-	// 		}));
-
-
-	// const nest = (items, id = null) => {
-	// 	items
-	// 		.filter((item) => item.parentReply === id)
-	// 		.map((item) => ({ ...item, children: nest(items, item.id) }));
-	// };
-
-	// const comments = question.replies;
-
-	// const [nestedComments, setNestedComments] = useState(comments);
 	const nestedComments = props.nestedComments
-
-	console.log("Nested Comments: " + nestedComments);
-
-	// const Comment = (props) => (
-	// 	<li>
-	// 		{props.id}
-	// 		<ul>
-	// 			{props.children.map((child) => (
-	// 				<Comment {...child} />
-	// 			))}
-	// 		</ul>
-	// 	</li>
-	// );
-
-	// const List = ({ comments }) => (
-	// 	<ul>
-	// 		{comments.map((comment) => (
-	// 			<Comment {...comment} />
-	// 		))}
-	// 	</ul>
-	// );
-
 	return (
 		<CustomDiv>
 			<CustomUnorderedList>
